c5e522f8df47f633e245211db348289ce0ad7a74
Alexandro Sousa <alexandrosousauitec@gmail.com>
2025-08-19 16:38:43 -0300

    ajustes(jogo rodando

--- code/settings.py
+++ code/settings.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 
 LARGURA_TELA = 480
 ALTURA_TELA = 600
@@ -9,6 +10,13 @@ FPS = 60
 PRETO = (0, 0, 0)
 BRANCO = (255, 255, 255)
 
+def resource_path(relative_path):
+    """ Retorna o caminho absoluto para o asset, funciona para dev e para PyInstaller """
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-diretorio_jogo = os.path.dirname(os.path.dirname(__file__)) 
-diretorio_assets = os.path.join(diretorio_jogo, 'assets')
+    return os.path.join(base_path, relative_path)
+
+diretorio_assets = resource_path("assets")
